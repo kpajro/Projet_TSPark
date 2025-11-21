82153e0c7f8c33ee082d6687a09be215dc5376ce
leosu1 <leopirrotta14@gmail.com>
2025-11-21 16:01:20 +0100

    mise en place du projet (install express + structure + index.ts)

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(400).send('hello');
+})
+
+app.listen(port, () => {console.log(`App running locally at localhost:${port}`)});
