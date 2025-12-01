eeec14f3988474975970df01f96e5e60bcb40348
leosu1 <leopirrotta14@gmail.com>
2025-12-01 10:08:22 +0100

    ajout de la config pour dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import "dotenv";
+import { config } from "dotenv";
 
 const app = express();
 const port = 3000;
+config({quiet: true, path: '../.env'});
 
 app.get('/', (req, res) => {
     res.status(200).send('hello');
