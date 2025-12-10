e5922de5f696b0b5ecb2971b9e5bf2e3c1e2dfd3
pajro <147093238+kpajro@users.noreply.github.com>
2025-12-10 01:43:41 +0100

    index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import express from "express";
 import { config } from "dotenv";
+import express from "express";
+import * as controllers from "./Controllers"
+import { builder } from "./Utils"
 
+config({quiet: true, path: '.env'});
 const app = express();
 const port = 3000;
-config({quiet: true, path: '../.env'});
+
+app.use(express.json())
+builder(app, controllers)
 
 app.get('/', (req, res) => {
     res.status(200).send('hello');
